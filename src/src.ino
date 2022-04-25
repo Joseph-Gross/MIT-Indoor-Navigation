544aae94100af4f4fb6ce6a78e44b0c481e98781
menken <menken@mit.edu>
2022-04-25 16:26:51 -0400

    Fleshed out some more of src.ino. Still a lot to do

--- src/src.ino
+++ src/src.ino
@@ -8,27 +8,49 @@
 #include <Adafruit_ST7735.h> // Hardware-specific library NEEDED FOR COMPASS SENSING
 #include <math.h>
 #include <Wire.h>
+#include <Button.h>
+#include <Compass.h>
+#include <Navigation.h>
+#include <DestinationSelection.h>
+
+#define BACKGROUND TFT_BLACK
 
 // Define global variables
 TFT_eSPI tft = TFT_eSPI();
 // Button on pin 45
 const int BUTTON = 45;
 int32_t timer;
+char dest[20];
+char prev_dest[20] = "";
+
 
 
 enum global_state {START, ROOM_SELECT, CONFIRM_DESTINATION, NAVIGATING, CONFIRM_CANCEL, ARRIVED};
 
 global_state state = START;
+global_state previous_state = ARRIVED;
+
+Button button(BUTTON); //button object!
+Navigation navigator(); //navigation object
+Compass compass(); //compass object
+
+
+
 void global_update(int button){
   // As a convention, try to call display_functionwhatever() only when transitioning between states if it makes sense (this won't work for some of them)
   switch (state){
     case START:
       /*
-      This is the idle state. A long press will move us to room selection mode.
+      This is the idle state. Any press will move us to room selection mode.
       */
+      if (previous_state!=START){
+        tft.fillScreen(BACKGROUND);
+        tft.println("Welcome to TinyTim, MIT's premiere campus navigator\n");
+        tft.println("Press the button to activate your guide and select a destination.");
+        previous_state = START;
+      }
       if (button != 0) state = ROOM_SELECT;
     break;
-
 
     case ROOM_SELECT:
       /* 
@@ -36,9 +58,23 @@ void global_update(int button){
       After both a building and room are selected with short presses, 
       we move to the Confirm Destination state.
       */
-
+      if (previous_state!=ROOM_SELECT){
+        tft.fillScreen(BACKGROUND);
+        tft.println("Tilt screen for number scrolling. \n");
+        tft.println("Short press to confirm destination. \n");
+        previous_state = ROOM_SELECT;
+      }
+      // dest = select_destination();
+      // if (strcmp(dest,prev_dest)!=0){
+        // tft.setCursor(20,0,1); // whatever cursor location prevents it from 
+        // tft.println(dest);
+        // prev_dest = dest;
+      // }
+      
+      if (button != 0) {
+        state = CONFIRM_DESTINATION;
+      }
     break;
-
 
     case CONFIRM_DESTINATION:
       /* 
@@ -46,7 +82,15 @@ void global_update(int button){
       and we move to navigating, or a long press canceling the destination, 
       and taking us back to room selection
       */
-
+      if (previous_state!=CONFIRM_DESTINATION){
+        tft.fillScreen(BACKGROUND);
+        tft.println("You selected the following destination \n");
+        tft.println(dest);
+        tft.println('\nShort press to continue. Long press to reselect.\n')
+        previous_state = CONFIRM_DESTINATION;
+      }
+      if (button == 1) state = NAVIGATING;
+      if (button == 2) state = ROOM_SELECT;
     break;
 
 
@@ -57,8 +101,21 @@ void global_update(int button){
       to the arrived state, or the user long presses to cancel the current 
       navigation and moves to the Confirm Cancel Navigation state. 
       */
-
+      if (previous_state!=NAVIGATING){
+        tft.fillScreen(BACKGROUND);
+        tft.println("Navigation in progress \n");
+        tft.println(dest);
+        tft.println('\nLong press button to exit.\n')
+        previous_state = NAVIGATING;
+      }
+      // figure out current location
+      // navigator.curr_loc(); // or whatever this function would be
+      // make an http request for the path
+      // response = navigator.http_request(dest, other_paramters);
+      // char parsed[100] = navigator.parse_response() // or whatever function does this and whatever the variable should contain
+      // compass.update(dir_next_node, distance); // this should call arrow.update() internally? how will this display?
       if (button == 2) state = CONFIRM_CANCEL;
+      // if (navigation.arrived()) state = ARRIVED; // or whatever function does this.
     break;
 
 
@@ -68,8 +125,13 @@ void global_update(int button){
       canceling the navigation and we move to Start state, 
       or a long press cancels the navigation cancel.
       */
-
-
+      if (previous_state!=CONFIRM_CANCEL){
+        tft.fillScreen(BACKGROUND);
+        tft.println("Exit navigation? Short press if yes, long press if no. \n");
+        previous_state = CONFIRM_CANCEL;
+      }
+      if (button == 1) state = START;
+      if (button == 2) state = NAVIGATING;
     break;
 
 
@@ -78,12 +140,17 @@ void global_update(int button){
       In the arrived state, we will display an arrived 
       message until a short press moves us back to the Start state.  
       */
-
+      if (previous_state!=ARRIVED){
+        tft.fillScreen(BACKGROUND);
+        tft.println("You have arrived! Press button to return to start. \n");
+        previous_state = ARRIVED;
+      }
+      if (button != 0) state = START;
     break;
   }
 }
 
-Button button(BUTTON); //button object!
+
 
 void setup(){
   Serial.begin(115200); // Set up serial port
