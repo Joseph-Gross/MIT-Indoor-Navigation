557c0915af242b89e83141103c69bef67c11937a
Adam Snowdon <asnowdon@mit.edu>
2022-04-27 22:19:00 -0400

    make roomselection work as a class

--- src/DestinationSelection.h
+++ src/DestinationSelection.h
@@ -15,9 +15,13 @@ class DestinationSelection {
     destination_selection_state state;
     char destination[MAX_BUILDING_NAME_LENGTH];
     int destination_floor;
+    char output[1000]; //char array buffer to hold HTTP request
+    char buildings[50] = "010305071113101204060208162638";
+    char floors[4] = "01";
+
 public:
     DestinationSelection();
-    int update();
+    void update();
     void display();
     char* get_destination();
     int get_destination_floor();
